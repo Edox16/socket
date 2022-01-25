d4ab8ffdcafd3437c8bf64af35ae3eed892820b7
Edox16 <92153329+Edox16@users.noreply.github.com>
2022-01-25 09:28:24 +0100

    Nuovo Es Socket contatore

---
+++ contatore_server.py
@@ -0,0 +1,36 @@
+#Importo il socket ed il json
+import socket
+import json
+
+#Dichiaro una porta e gli assegno un valore (127.0.0.1) e dopo una porta (65433)
+HOST="127.0.0.1"
+PORT=65433
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    cont=1
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind((HOST,PORT))
+    s.listen()
+    print("[*] In ascolto con %s:%d"%(HOST,PORT))
+    clientsocket, address = s.accept()
+    with clientsocket as cs:
+        print("Connessione da ", address)
+        while True:
+            data=cs.recv(1024)
+            if not data:
+                break
+            data=data.decode()
+            data=json.loads(data)
+            stri=data['Stringa']
+            if stri != "KO":
+                risposta="Numero del messaggio: " + str(cont) + "; " + stri
+                cont += 1
+            else:
+                risposta='Ricevuto "KO" dal server. Chiudo la connessione con il client.'
+            cs.sendall(risposta.encode("UTF-8"))
+
+
+
+
+
+
